def7c98e1c70cf69a3c44ad125c4038af2d50717
albaabhusain <husainalbaab2000@gmail.com>
2021-08-10 14:07:39 +0530

    minor:navbar update

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const line3=document.querySelector('.l-3')
 resNav.addEventListener('click',()=>{
 
    navList.classList.toggle("resnav");
+   resNav.classList.toggle('fixed');
    line1.classList.toggle('line-1');
    line2.classList.toggle('d-none');
    line3.classList.toggle('line-2');
