629a9f822bd6f6912b0455987e5c9737a11c09a7
albaabhusain <husainalbaab2000@gmail.com>
2021-08-10 13:51:43 +0530

    Feat : animate resposive and section 2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 const resNav= document.querySelector('.burgerNav');
 const navList = document.querySelector('.navList');
 const sect1= document.querySelector('.section-1');
-
-
+const line1=document.querySelector('.l-1');
+const line2=document.querySelector('.l-2');
+const line3=document.querySelector('.l-3')
 
 
 
@@ -10,7 +11,11 @@ const sect1= document.querySelector('.section-1');
 resNav.addEventListener('click',()=>{
 
    navList.classList.toggle("resnav");
-   sect1.classList.toggle('d-none');
+   line1.classList.toggle('line-1');
+   line2.classList.toggle('d-none');
+   line3.classList.toggle('line-2');
+
+   
 
 })
 
